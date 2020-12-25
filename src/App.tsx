c8ff66a0ce9853a79ff9e4243acf4cbc89333348
Edward <e.arechwa@gmail.com>
2020-12-25 19:52:34 +0100

    add logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,52 @@
 import Form from './components/form';
 import Nav from './components/nav';
 import List from './components/list';
+import {useEffect, useState} from 'react';
+import {ITodo} from './components/interfaces';
 
 const App = () => {
+  const [todos, setTodos] = useState<Array<ITodo>>([]);
+  useEffect(() => {
+    setTodos(JSON.parse(localStorage.getItem('todos') || '[]'));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (value: string) => {
+    const data: ITodo = {
+      value,
+      id: Date.now(),
+      completed: false,
+    };
+    setTodos((prevState) => [data, ...prevState]);
+  };
+  const completeTodo = (id: number) => {
+    setTodos((prevState) =>
+      prevState.map((item) => {
+        if (item.id === id) {
+          return {...item, completed: !item.completed};
+        }
+        return item;
+      })
+    );
+  };
+
+  const deleteTodo = (id: number) => {
+    setTodos((prevState) => prevState.filter((item) => item.id !== id));
+  };
+
   return (
     <>
       <Nav />
       <div className="container">
-        <Form />
-        <List />
+        <Form onFormSubmit={addTodo} />
+        <List
+          data={todos}
+          clickHandler={completeTodo}
+          deleteHandler={deleteTodo}
+        />
       </div>
     </>
   );
--- src/components/form/form.tsx
+++ src/components/form/form.tsx
@@ -1,17 +1,40 @@
-const Form = () => {
+import {useState} from 'react';
+
+type FormProps = {
+  onFormSubmit: (value: string) => void;
+};
+
+const Form: React.FC<FormProps> = (props) => {
+  const {onFormSubmit} = props;
+  const [value, setValue] = useState('');
+
+  const onValueChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(evt.target.value);
+  };
+
+  const submitHandler = (evt: React.FormEvent<HTMLFormElement>) => {
+    evt.preventDefault();
+    onFormSubmit(value);
+    setValue('');
+  };
+
   return (
-    <>
-      <div className="col s8">
-        <form className="col">
-          <div className="input-field">
-            <input value="" id="todo-field" type="text" className="validate" />
-            <label className="active" htmlFor="todo-field">
-              Your ToDo
-            </label>
-          </div>
-        </form>
-      </div>
-    </>
+    <div className="col s8">
+      <form className="col" onSubmit={submitHandler}>
+        <div className="input-field">
+          <input
+            value={value}
+            id="todo-field"
+            type="text"
+            className="validate"
+            onChange={onValueChange}
+          />
+          <label className="active" htmlFor="todo-field">
+            Your ToDo
+          </label>
+        </div>
+      </form>
+    </div>
   );
 };
 
